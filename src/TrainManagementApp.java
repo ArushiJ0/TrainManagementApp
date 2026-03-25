9f5fdf6fae4e936e2e7980ffca677741f9af8fdd
ArushiJ0 <arushi.123253@gmail.com>
2026-03-25 10:49:31 +0530

    UC1

--- src/TrainManagementApp.java
+++ src/TrainManagementApp.java
@@ -1,2 +1,11 @@
-public class TrainManagementApp {
+import java.util.List;
+import java.util.ArrayList;
+
+public class TrainManagementApp {  public static void main (String [] args){
+    System.out.println("=========================================");
+    System.out.println("========Train Consist Management App=======");
+    System.out.println("==========================================");
+    List<String> trainConsist = new ArrayList<>();
+    System.out.println("Train initialised successfully");
+}
 }
